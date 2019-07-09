81f101c4da68223d579ea30fde3784e5160f420a
logotie <phelpsakinsan@gmail.com>
2019-07-09 13:46:33 +0100

    Changed started url and fixed routing

--- GamesApi/Controllers/GamesController.cs
+++ GamesApi/Controllers/GamesController.cs
@@ -11,7 +11,7 @@ using Newtonsoft.Json;
 
 namespace GamesApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class GamesController : ControllerBase
     {

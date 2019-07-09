74b2d02bba8d8cc0ca932ec4e6d84e9fe498466a
logotie <phelpsakinsan@gmail.com>
2019-07-09 11:31:33 +0100

    Added tests for games api service

---
+++ GamesApiXunitTests/GamesApiServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using GamesApi.Db;
+using GamesApi.Models.Game;
+using GamesApi.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GamesApiXunitTests
+{
+    public class GamesApiServiceTests
+    {
+        //Readonly because it shouldn't change
+        private readonly IGamesApiService gamesApiService;
+
+        //We use Mock to quickly setup the object
+        private Mock<IMongoDbContext> mockMongoDb;
+        private Mock<Comment> mockComment, mockComment2, mockComment3;
+        private List<Comment> mockListComment, mockListComment2;
+        private Mock<Game> mockGame, mockGame2;
+
+        private readonly int GAMEID = 2;
+        private readonly int GAMEID2 = 3;
+
+        public GamesApiServiceTests()
+        {
+            mockComment = new Mock<Comment>();
+            mockComment2 = new Mock<Comment>();
+            mockComment3 = new Mock<Comment>();
+
+            mockListComment = new List<Comment>
+            {
+                mockComment.Object, mockComment2.Object
+            };
+
+            mockListComment2 = new List<Comment>
+            {
+                mockComment3.Object
+            };
+
+            mockGame = new Mock<Game>();
+            mockGame2 = new Mock<Game>();
+
+            mockMongoDb = new Mock<IMongoDbContext>();
+            mockMongoDb.
+                Setup(mockMongo => mockMongo.Get()).
+                Returns(new List<Game> { mockGame.Object, mockGame2.Object });
+
+            gamesApiService = new GamesApiService(mockMongoDb.Object);
+        }
+
+        [Fact]
+        public void RetrieveGameByIdTest()
+        {
+            Game game = gamesApiService.RetrieveGameById(GAMEID);
+            Assert.True(game.id == GAMEID);
+        }
+    }
+}

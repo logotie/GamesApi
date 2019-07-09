d38e5d289ab6ce9660a3b194e426b5fde2213ee2
logotie <phelpsakinsan@gmail.com>
2019-07-09 14:26:23 +0100

    Got the mongo db working, used IQueryable when suitable.  Therefore the query will be lazy loaded and only executed when needed.

--- GamesApi/Db/MongoDbContext.cs
+++ GamesApi/Db/MongoDbContext.cs
@@ -6,6 +6,7 @@ using GamesApi.Helper;
 using GamesApi.Models.Game;
 using Mongo2Go;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 using Newtonsoft.Json;
 
 namespace GamesApi.Db
@@ -13,43 +14,54 @@ namespace GamesApi.Db
     //Db context for local mongo db instance, handles data access operations
     public class MongoDbContext : IMongoDbContext
     {
-        private readonly IList<Game> _games;
+        private readonly IQueryable<Game> _games;
         private MongoDbRunner _runner;
 
-        //Going to use this for now, then I will add Mongo Db later
+        //Mongo Db local instance 
         public MongoDbContext()
         {
             _runner = MongoDbRunner.StartForDebugging();
 
             MongoClient client = new MongoClient(_runner.ConnectionString);
+            //TODO Change this
             MongoServer server = client.GetServer();
             MongoDatabase database = server.GetDatabase("Games");
+
+            //Get the collection for writing
             var collection = database.GetCollection<Game>("GameItems");
+
+            //We want to know that it has been written successfully
             collection.WithWriteConcern(WriteConcern.Acknowledged);
 
+            //Retrieve the game objects from the json
             List<Game> games = RetrieveObjectsFromJson();
 
- 
             foreach(Game game in games)
             {
                 try
                 {
+                    //Attempt to add each object to the mango db
                     collection.Insert<Game>(game);
                 }
                 catch(Exception e)
                 {
-                    //Should crash
-                    //TODO decide how to to handle this
+                    Console.WriteLine("Unable to write to MongoDb, game Id:" + game.id);
                 }
             }
 
+            //IQueryable uses lazy loading so it is not actually reading the entire list to memory
+            //It will only actually load the data when 'tolist' or the query is explicitly executed
+            _games = collection.AsQueryable<Game>();
         }
 
+        //Converts the queryable to a list therefore the queryable will be converted from 
+        //an expression tree to the game objects in memory
         public List<Game> Get()
         {
             return _games.ToList();
         }
 
+        //Returns the game id 
         public Game GetById(int id)
         {
             return _games.FirstOrDefault(game => game.id == id);

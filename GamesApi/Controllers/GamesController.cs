027ff88531da6d0e571271f5ac1be379d8716da6
logotie <phelpsakinsan@gmail.com>
2019-07-09 12:54:28 +0100

    Added query param search api

--- GamesApi/Controllers/GamesController.cs
+++ GamesApi/Controllers/GamesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GamesApi.Models.Game;
+using GamesApi.Models.Url;
 using GamesApi.Services;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -35,6 +36,46 @@ namespace GamesApi.Controllers
 
             //Return an indented json string to be displayed to the user
             String jsonString = JsonConvert.SerializeObject(game.gameDetails, Formatting.Indented);
+
+            return jsonString;
+        }
+
+        /// <summary>
+        /// Allows a user to search for a game using any of the params listed below
+        /// </summary>
+        /// <param name="queryParams">
+        /// likesGreaterThan
+        /// platform
+        /// publisher
+        /// publishedAfter
+        /// minAgeRating
+        /// maxAgeRating
+        /// minCommentsAmount
+        /// </param>
+        /// The queries will be in the form of:
+        /// api/games?minagerating=5&likesgreaterthan=4
+        /// <returns></returns>
+        // GET api/games?minagerating=5&likesgreaterthan=4
+        [HttpGet()]
+        public ActionResult<string> QuerySearch([FromQuery] GameQueryParams queryParams)
+        {
+            if (queryParams == null)
+            {
+                return BadRequest();
+            }
+
+            List<Game> games = gamesApiService.SearchGamesByQueryParams(queryParams);
+
+            if (games == null)
+            {
+                return "";
+            }
+
+            List<GameDetails> gameDetails = new List<GameDetails>();
+
+            games.ForEach(game => gameDetails.Add(game.gameDetails));
+
+            string jsonString = JsonConvert.SerializeObject(gameDetails, Formatting.Indented);
 
             return jsonString;
         }

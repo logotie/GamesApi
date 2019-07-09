0dc2014c12dfd184ee2e7c80e760d5dbc44a2ccd
logotie <phelpsakinsan@gmail.com>
2019-07-09 13:08:12 +0100

    Added models for report

---
+++ GamesApi/Models/Report/AvgLikesReport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesApi.Models.Report
+{
+    public class AvgLikesReport
+    {
+        public string title { get; set; }
+        public int average_likes { get; set; }
+    }
+}
---
+++ GamesApi/Models/Report/ReportComplete.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesApi.Models.Report
+{
+    public class ReportComplete
+    {
+        public string user_with_most_comments { get; set; }
+        public string highest_rated_game { get; set; }
+        public List<AvgLikesReport> average_likes_per_game { get; set; }
+    }
+}

1b3635ef4532f45f9bbf927c69375f97377c49a5
logotie <phelpsakinsan@gmail.com>
2019-07-09 10:32:17 +0100

    Add project files.

---
+++ GamesApi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace GamesApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //The host is responsible for start up and the lifetime of the web app
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        //Builds the host with certain config.
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}

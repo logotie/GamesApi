53b274c5da037f4522b8d3af17a531ae9ce51206
logotie <phelpsakinsan@gmail.com>
2019-07-09 10:57:00 +0100

    Adding model objects for the 'game' values api functionality

---
+++ GamesApi/Models/Game/Comment.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesApi.Models.Game
+{
+    public class Comment
+    {
+        public virtual string user { get; set; }
+        public virtual string message { get; set; }
+        public virtual string dateCreated { get; set; }
+        public virtual int like { get; set; }
+    }
+}

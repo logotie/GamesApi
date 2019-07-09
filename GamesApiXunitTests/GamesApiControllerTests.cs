86db3be8122a4fd677cf43b63e383bb02c45aba3
logotie <phelpsakinsan@gmail.com>
2019-07-09 10:42:06 +0100

    Added test project

---
+++ GamesApiXunitTests/GamesApiControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GamesApiXunitTests
+{
+    public class GamesApiControllerTests
+    {
+        [Fact]
+        public void GetByIdApiTest()
+        {
+
+        }
+    }
+}

2f662bf14755a22d5e463216c41375305181b828
logotie <phelpsakinsan@gmail.com>
2019-07-09 14:07:13 +0100

    Adding mongo db but have to add Object Ids to models

--- GamesApi/Db/MongoDbContext.cs
+++ GamesApi/Db/MongoDbContext.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using GamesApi.Helper;
 using GamesApi.Models.Game;
+using Mongo2Go;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 
 namespace GamesApi.Db
@@ -12,11 +14,35 @@ namespace GamesApi.Db
     public class MongoDbContext : IMongoDbContext
     {
         private readonly IList<Game> _games;
+        private MongoDbRunner _runner;
 
         //Going to use this for now, then I will add Mongo Db later
         public MongoDbContext()
         {
-            _games = RetrieveObjectsFromJson();
+            _runner = MongoDbRunner.StartForDebugging();
+
+            MongoClient client = new MongoClient(_runner.ConnectionString);
+            MongoServer server = client.GetServer();
+            MongoDatabase database = server.GetDatabase("Games");
+            var collection = database.GetCollection<Game>("GameItems");
+            collection.WithWriteConcern(WriteConcern.Acknowledged);
+
+            List<Game> games = RetrieveObjectsFromJson();
+
+ 
+            foreach(Game game in games)
+            {
+                try
+                {
+                    collection.Insert<Game>(game);
+                }
+                catch(Exception e)
+                {
+                    //Should crash
+                    //TODO decide how to to handle this
+                }
+            }
+
         }
 
         public List<Game> Get()

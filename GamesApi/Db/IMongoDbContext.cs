7a18a294072bb3eb79f053529dc67143841ce3b2
logotie <phelpsakinsan@gmail.com>
2019-07-09 11:04:46 +0100

    Added interface methods, not yet implemented

--- GamesApi/Db/IMongoDbContext.cs
+++ GamesApi/Db/IMongoDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GamesApi.Models.Game;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace GamesApi.Db
     //Interface for the MongoDb local database, allows me to moq the object later on
     public interface IMongoDbContext
     {
-
+        //Retrieve all the games
+        List<Game> Get();
+        //Retrieve the game by id
+        Game GetById(int id);
     }
 }
--- GamesApi/Db/MongoDbContext.cs
+++ GamesApi/Db/MongoDbContext.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GamesApi.Models.Game;
 
 namespace GamesApi.Db
 {
     //Db context for local mongo db instance, handles data access operations
-    public class MongoDbContext:IMongoDbContext
+    public class MongoDbContext : IMongoDbContext
     {
+        public List<Game> Get()
+        {
+            throw new NotImplementedException();
+        }
 
+        public Game GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

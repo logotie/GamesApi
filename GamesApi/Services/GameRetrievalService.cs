35aa43fdac9090d7ff28e9eb7dedd4b30c3bf241
logotie <phelpsakinsan@gmail.com>
2019-07-09 12:38:32 +0100

    Adding logic to un-implemented methods

--- GamesApi/Services/GameRetrievalService.cs
+++ GamesApi/Services/GameRetrievalService.cs
@@ -29,20 +29,57 @@ namespace GamesApi.Services
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// Retrieves the game that has the highest sum of likes from all comments
+        /// </summary>
+        /// <returns>The Game object with the highest sum of comment likes</returns>
         public Game RetrieveByHighestSumOfLikes()
         {
-            throw new NotImplementedException();
-        }
+            //Stores found users and their likes count
+            Dictionary<int, int> userAndLikesCount = new Dictionary<int, int>();
 
+            //Go through each comment in each game, keeping a count of each user found and likes count
+            foreach (Game game in context.Get())
+            {
+                foreach (Comment comment in game.gameDetails.comments)
+                {
+                    int gameId = game.id;
+
+                    if (userAndLikesCount.ContainsKey(gameId))
+                    {
+                        userAndLikesCount[gameId] = userAndLikesCount[gameId] + 1;
+                    }
+                    else
+                    {
+                        userAndLikesCount.Add(gameId, 1);
+                    }
+                }
+            }
+
+            //Order the list by descending order
+            int ordered = userAndLikesCount.OrderBy(x => x.Value).FirstOrDefault().Key;
+
+            //Get the first result or return null if not found
+            return context.Get().FirstOrDefault(x => x.id == ordered);
+        }
+        /// <summary>
+        /// Retrieve the game that matches the specific id or return null if not found
+        /// </summary>
+        /// <param name="id">id of the game</param>
+        /// <returns>Game object with the matching id</returns>
         public Game RetrieveById(int id)
         {
             return context.Get().FirstOrDefault(game => game.id == id);
         }
-
+        /// <summary>
+        /// Retrieves the game object where the 'like' field of the game details, is greater than the parameter integer
+        /// </summary>
+        /// <param name="minAmountOfLikes">Minimum amount of likes that a game must have</param>
+        /// <param name="games">Games that a greater amount of likes than the parameter</param>
+        /// <returns></returns>
         public List<Game> RetrieveByLikesGreaterThan(int minAmountOfLikes, List<Game> games)
         {
-            throw new NotImplementedException();
+            return games.Where(game => game.gameDetails.likes >= minAmountOfLikes).ToList();
         }
 
         public List<Game> RetrieveByMaxAge(int maxAgeRating, List<Game> games)

fc6d5c8e3a6c93701e645a41d56d17be275ab9bd
logotie <phelpsakinsan@gmail.com>
2019-07-09 11:38:03 +0100

    Added helper class for formatting

---
+++ GamesApi/Helper/DbHelper.cs
@@ -0,0 +1,39 @@
+﻿using GamesApi.Models.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesApi.Helper
+{
+    public class DbHelper
+    {
+        /// <summary>
+        /// The dates are stored in the db as a long from epoch
+        /// We convert the long to 'yyyy-mm-dd'
+        /// </summary>
+        /// <param name="games">Game objects from the db, sourced from a JSON</param>
+        //TODO Add unit test for this method
+        public static void FormatCommentDatesForUi(List<Game> games)
+        {
+            games.
+                ForEach(game => game.gameDetails.comments.ToList().
+                ForEach(comment => comment.dateCreated = 
+                convertLongToDateTimeString(Convert.ToDouble(comment.dateCreated))));
+        }
+
+        /// <summary>
+        /// Converts long to 'yyyy-mm-dd'
+        /// </summary>
+        /// <param name="timeAfterEpoch"></param>
+        /// <returns></returns>
+        private static string convertLongToDateTimeString(double timeAfterEpoch)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(timeAfterEpoch));
+
+            string a = dateTimeOffset.DateTime.ToString("yyyy-MM-dd");
+
+            return a;
+        }
+    }
+}
